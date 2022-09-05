0adde5c2b40d1b7809fab209915ceca3dcc27ce0
azotamiota <azotamiota@gmail.com>
2022-09-05 23:16:07 +0100

    socket.io bug fixed

--- app.js
+++ app.js
@@ -1,40 +1,15 @@
 const express = require('express')
 const app = express()
 const cors = require('cors')
-const http = require('http')
-const {Server} = require('socket.io')
-require('dotenv').config()
 
 const userController = require('./controllers/user')
 
-
 app.use(cors())
+
 app.use(express.json())
 
 app.get('/', (reg, res) => {res.json({message: 'Welcome to utilitiy billionare backend'})})
 app.post('/results', userController.sendResults)
 app.get('/leaderboard', userController.fetchLeaderboard)
-
-const server = http.createServer(app)
-
-
-// const url = 'https://utility-billionare.netlify.app'
-const url = 'http://192.168.0.14:5173/waiting'
-
-const io = new Server(server, {
-    cors: {
-        origin: url,
-        methods: ['GET', 'POST']
-    }
-})
-
-io.on('connection', (socket) => {
-    console.log(`User Connected: ${socket.id}`);
-    socket.on('disconnect', () => {
-        console.log(`User ${socket.id} disconnected`);
-    })
-
-})
-
 
 module.exports = app;
--- index.js
+++ index.js
@@ -1,6 +1,32 @@
-const server = require('./app')
+const app = require('./app')
+const http = require('http')
+const {Server} = require('socket.io')
 const connectDB = require('./db/connect')
 require('dotenv').config()
+
+const server = http.createServer(app)
+
+// const url = 'https://utility-billionare.netlify.app'
+const url = 'http://192.168.0.14:5173'
+
+const io = new Server(server, {
+    cors: {
+        origin: url,
+        methods: ['GET', 'POST']
+    }
+})
+
+io.on('connection', (socket) => {
+    console.log(`User Connected: ${socket.id}`);
+    socket.on('disconnect', () => {
+        console.log(`User ${socket.id} disconnected`);
+    })
+    socket.on("send_message", (data) => {
+        console.log('data arrived, to be emitted: ', data);
+        io.emit('receive_message', data)
+    })
+
+})
 
 const port = process.env.PORT || 5000
 
