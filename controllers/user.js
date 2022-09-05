5beba0fe07c92004af5a5a299455310e3d6d157d
azotamiota <azotamiota@gmail.com>
2022-09-05 11:40:22 +0100

    all functionality done

--- controllers/user.js
+++ controllers/user.js
@@ -5,9 +5,19 @@ const sendResults = async (req, res) => {
     try {
         const username = req.body.username
         const score = req.body.score
-        console.log(username, score)
-        const user = await User.create({'username': username, 'score' : score})  
-        res.status(201).json({username: username,  message: 'User created'})                
+
+        const isExistingUser = await User.findOne({username});
+        if (isExistingUser) {
+            console.log('isExistingUser: ', isExistingUser)
+            isExistingUser.score += score
+            isExistingUser.save()
+            res.status(201).json({username: isExistingUser.username, message: 'score updated', 'score added': score})
+        } else {
+            console.log('New user detected');
+            await User.create({'username': username, 'score' : score})  
+            res.status(201).json({username: username,  message: 'user created', 'new-score': score})
+        }
+                        
     } catch (error) {                       
         res.status(500).json({message: error})
     }
