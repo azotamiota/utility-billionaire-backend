78a9ff9011c81e07f475fc89f44ebc823aa7c670
azotamiota <azotamiota@gmail.com>
2022-09-06 18:45:10 +0100

    add temp file

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ require('dotenv').config()
 const server = http.createServer(app)
 
 // const url = 'https://utility-billionare.netlify.app'
-const url = 'http://192.168.1.23:5173'
+const url = 'http://192.168.0.14:5173'
 let players = []
 let questions = []
 const io = new Server(server, {
